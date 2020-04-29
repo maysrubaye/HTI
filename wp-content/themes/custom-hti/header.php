2ae750391a39b1d92c566e85a3cd39be54d93aaf
mays <mrubaye@terpmail.umd.edu>
2020-04-29 17:06:36 -0400

    modified search form

--- wp-content/themes/custom-hti/header.php
+++ wp-content/themes/custom-hti/header.php
@@ -27,11 +27,7 @@
 
 			<!-- header-search -->
 			<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-		    <!-- <div> -->
-		    	<!-- <label class="screen-reader-text" for="s">Search for:</label> -->
 		        <input type="text" value="<?php the_search_query()?>" name="s" id="s" placeholder="Search the Site"/>
-		        <!-- <input type="submit" id="searchsubmit" value="Search" /> -->
-		    <!-- </div> -->
 			</form> 
         	<!-- /header-search -->
 		</nav>

e69fba9558aca28e69e0dee900499ca15dc798c1
mays <mrubaye@terpmail.umd.edu>
2020-04-29 12:52:15 -0400

    removed extra h2 and fixed submenu on click

--- wp-content/themes/custom-hti/page.php
+++ wp-content/themes/custom-hti/page.php
@@ -6,8 +6,6 @@ get_header();
 <?php
 if (have_posts()):
 	while(have_posts()) : the_post(); ?>
-
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<?php the_content(); ?>
 	<?php endwhile;
 

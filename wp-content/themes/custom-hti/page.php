4820ef3d6b2a3f3cf302bce375e02bde70c96ade
mays <mrubaye@terpmail.umd.edu>
2020-05-01 02:33:46 -0400

    adjusted width for regular pages and event pages

--- wp-content/themes/custom-hti/page.php
+++ wp-content/themes/custom-hti/page.php
@@ -1,20 +1,16 @@
 <?php 
 get_header(); 
 ?>
-
-<div class="container">
 <?php
 if (have_posts()):
-	while(have_posts()) : the_post(); ?>
-		<?php the_content(); ?>
-	<?php endwhile;
+	while(have_posts()) : the_post();
+		the_content();
+	endwhile;
 
 else:
 	echo '<p>no posts to show</p>';
-
 endif;
 ?>
-</div>
 <?php
 get_footer();
 ?>

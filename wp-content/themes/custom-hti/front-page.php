ca24b99fdf6b10e82bd654a072c0f8807aad6388
mays <mrubaye@terpmail.umd.edu>
2020-05-16 21:22:40 -0400

    final modifications

--- wp-content/themes/custom-hti/front-page.php
+++ wp-content/themes/custom-hti/front-page.php
@@ -3,8 +3,6 @@
 * Template Name: Home Page Template
 */
 get_header(); 
-		echo "string";
-
 		$query = new WP_Query( array( 'meta_key' => '_is_ns_featured_post', 'meta_value' => 'yes' ) );
 			if ( $query->have_posts() ) :
 				while ( $query->have_posts() ) : $query->the_post();
